6065984fde52051bb5ec9480c3e550c103ff9e18
Zaid Khan <zaidbinasif468@gmail.com>
2026-02-19 10:35:18 +0500

    i will create it by my own effort

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// Ant Design reset styles
-import "antd/dist/reset.css";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
