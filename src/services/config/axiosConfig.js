fb770100d1e1925f1585aed8fc2868ed46694280
Zaid Khan <zaidbinasif468@gmail.com>
2026-02-23 15:15:16 +0500

    connected the remote server and db

--- src/services/config/axiosConfig.js
+++ src/services/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://06mtgz5m-5000.asse.devtunnels.ms/api",
     headers: {
         "Content-Type": "application/json",
     },
